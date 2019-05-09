0697de2a7b7d0d24f6976f9b5e91203153b0aac8
江志雄 <jiangzhixiong@jimistore.com>
2019-05-09 20:25:35 +0800

    测试

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ curl 'https://oapi.dingtalk.com/robot/send?access_token=200a5dd7f595b5afee3e7fe4
    -H 'Content-Type: application/json' \
    -d '{"msgtype": "text", 
         "text": {
-             "content": $COMMIT
+             "content": "$COMMIT"
         }
       }'
