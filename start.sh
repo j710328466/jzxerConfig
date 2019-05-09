88c1f22bcde650fa7cc2f91f439af840fc583059
江志雄 <jiangzhixiong@jimistore.com>
2019-05-09 20:45:16 +0800

    test

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
+echo $1 >/tmp/temp
 curl 'https://oapi.dingtalk.com/robot/send?access_token=200a5dd7f595b5afee3e7fe427915ea216fb39ebd9ad0447d037500378b495d9' \
    -H 'Content-Type: application/json' \
-   -d {"msgtype": "text", 
+   -d '{"msgtype": "text", 
         "text": {
-             "content": "我就是我, 是不一样的烟火"
+             "content": "`cat /tmp/temp`"
         }
-      }
+      }'
