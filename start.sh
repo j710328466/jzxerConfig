36bd319b5c7ee54e7654a6e7f0ec3e3134640fd7
江志雄 <jiangzhixiong@jimistore.com>
2019-05-09 20:46:30 +0800

    test

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-echo $1 >/tmp/temp
+echo $1 > /tmp/temp
 curl 'https://oapi.dingtalk.com/robot/send?access_token=200a5dd7f595b5afee3e7fe427915ea216fb39ebd9ad0447d037500378b495d9' \
    -H 'Content-Type: application/json' \
    -d '{"msgtype": "text", 
         "text": {
-             "content": "`cat /tmp/temp`"
+             "content": `cat /tmp/temp`
         }
       }'
