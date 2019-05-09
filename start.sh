7e44582910f0a0711c76b4e2134f661d2948dc07
江志雄 <jiangzhixiong@jimistore.com>
2019-05-09 17:18:43 +0800

    参数

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ curl 'https://oapi.dingtalk.com/robot/send?access_token=200a5dd7f595b5afee3e7fe4
    -H 'Content-Type: application/json' \
    -d '{"msgtype": "text", 
         "text": {
-             "content": $1
+             "content": '$1'
         }
       }'
