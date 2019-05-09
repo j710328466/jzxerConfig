f0e5a37f586072c8bff5b945b17da6300eb8017c
江志雄 <jiangzhixiong@jimistore.com>
2019-05-09 17:01:40 +0800

    创建脚本

---
+++ start.sh
@@ -0,0 +1,7 @@
+curl 'https://oapi.dingtalk.com/robot/send?access_token=200a5dd7f595b5afee3e7fe427915ea216fb39ebd9ad0447d037500378b495d9' \
+   -H 'Content-Type: application/json' \
+   -d '{"msgtype": "text", 
+        "text": {
+             "content": "我就是我, 是不一样的烟火"
+        }
+      }'
